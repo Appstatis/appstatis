713d85ecb9b35c9c7447b06cac4462c3eacd8054
paulbg <paulbg@hey.com>
2024-04-26 08:47:25 +0300

    feat: add featuresList data
    This data is decomposed from the features component to make it easier to maintain and update the features list.

---
+++ src/data/featuresList.ts
@@ -0,0 +1,35 @@
+import { SunIcon } from "@radix-ui/react-icons";
+import { Feature } from "@/types/Feature";
+
+export const featuresList: Feature[] = [
+  {
+    Icon: SunIcon,
+    title: "b",
+    description: "c",
+  },
+  {
+    Icon: SunIcon,
+    title: "b",
+    description: "c",
+  },
+  {
+    Icon: SunIcon,
+    title: "b",
+    description: "c",
+  },
+  {
+    Icon: SunIcon,
+    title: "b",
+    description: "c",
+  },
+  {
+    Icon: SunIcon,
+    title: "b",
+    description: "c",
+  },
+  {
+    Icon: SunIcon,
+    title: "b",
+    description: "c",
+  },
+];

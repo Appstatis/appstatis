5fae8f6a263cdc6a759ddfeb8e449388a87a6f07
paulbg <paulbg@hey.com>
2024-04-26 08:47:43 +0300

    feat: implement Features component
    This is a component that abstracts the logic of a single Feature and FeatureList.

--- src/components/features/features.tsx
+++ src/components/features/features.tsx
@@ -1,35 +1,9 @@
-import { Feature } from "./feature";
+import { FeaturesList } from "./featuresList";
+import { featuresList } from "../../data/featuresList";
 
+/**
+ * Features is a functional component that abstracts the logic of the FeaturesList component.
+ */
 export const Features = () => {
-  // dummy data
-  const featuresList = [
-    {
-      image: "https://via.placeholder.com/150",
-      title: "Feature 1",
-      description: "Description of feature 1",
-    },
-    {
-      image: "https://via.placeholder.com/150",
-      title: "Feature 2",
-      description: "Description of feature 2",
-    },
-    {
-      image: "https://via.placeholder.com/150",
-      title: "Feature 3",
-      description: "Description of feature 3",
-    },
-  ];
-
-  return (
-    <section className="grid gap-10 justify-center">
-      {featuresList.map((feature) => (
-        <Feature
-          key={feature.title}
-          image={feature.image}
-          title={feature.title}
-          description={feature.description}
-        />
-      ))}
-    </section>
-  );
+  return <FeaturesList featuresList={featuresList} />;
 };

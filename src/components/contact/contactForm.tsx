e03fd710b23c454bfc38b468a076409093b9ec23
paulbg <paulbg@hey.com>
2024-04-29 16:05:54 +0300

    feat: implement Textarea in contact form

--- src/components/contact/contactForm.tsx
+++ src/components/contact/contactForm.tsx
@@ -14,6 +14,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "../ui/textarea";
 
 const formSchema = z.object({
   name: z.string().min(2, {
@@ -122,7 +123,7 @@ export const ContactForm = () => {
             <FormItem>
               <FormLabel>Message</FormLabel>
               <FormControl>
-                <Input {...field} />
+                <Textarea {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>

e2e1dd4f0a90c6e3c346e54bcab4bb3309f13c8c
paulbg <paulbg@hey.com>
2024-04-29 10:58:13 +0300

    feat: implement scrolling to "features" in hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,39 +2,67 @@
 
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import anime from "animejs";
+import ScrollContext from "@/context/scrollContext";
+import { Button } from "./ui/button";
 
 export const Hero = () => {
+  const targetRef = useContext(ScrollContext);
+
+  const scrollToFeatures = () => {
+    if (targetRef.current) {
+      targetRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   useEffect(() => {
     anime
-      .timeline({ easing: "easeOutExpo" })
+      .timeline({ easing: "linear" })
       .add({
-        targets: ".header",
+        targets: ".unleash-ideas",
         opacity: [0, 1],
-        translateY: [-100, 0],
         duration: 1000,
       })
       .add({
-        targets: ".get-started",
+        targets: ".embrace-innovation",
         opacity: [0, 1],
-        translateY: [50, 0],
-        delay: anime.stagger(100),
+        duration: 1000,
+      })
+      .add({
+        targets: ".cta",
+        opacity: [0, 1],
+        duration: 1500,
+        delay: 800,
       });
   }, []);
 
   return (
     <>
       <section className="h-[75vh] grid justify-center place-content-center">
-        <div className="text-center space-y-3">
-          <header className="header">
-            <h1 className="font-bold text-3xl">appstatis</h1>
-            <p>Unleash ideas, Embrace Innovation</p>
+        <article className="space-y-3 max-w-2xl">
+          <header className="space-y-2 font-bold text-7xl">
+            <h1 className="unleash-ideas">Unleash ideas</h1>
+            <h2 className="embrace-innovation">Embrace Innovation</h2>
           </header>
-          <Link href="" className={`get-started ${buttonVariants()}`}>
-            Get Started
-          </Link>
-        </div>
+          <div className="space-y-3 cta">
+            <p className="message">
+              At <span className="text-xl font-bold">appstatis</span>, we
+              believe in the power of transformation through technology. Our
+              mission is to help you unleash your most ambitious ideas and turn
+              them into reality.
+            </p>
+            <Link href="" className={`mr-3 ${buttonVariants()}`}>
+              Get Started
+            </Link>
+            <Button onClick={scrollToFeatures} variant="outline">
+              Explore
+            </Button>
+          </div>
+        </article>
       </section>
     </>
   );

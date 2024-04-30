e2069c04d10fa470ea3ff6ce6efb6141a95f3f1b
paulbg <paulbg@hey.com>
2024-04-30 12:01:23 +0300

    feat: add services section to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "@/components/hero";
+import { Services } from "@/components/services";
 import { Features } from "@/components/features/features";
 import { FAQ } from "@/components/faq/faq";
 import { Pricing } from "@/components/pricing/pricing";
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
       <Features />
       <Pricing />
       <FAQ />

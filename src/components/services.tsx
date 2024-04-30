ce5a71df5872674824396db0dfaa95dd89c05d9d
paulbg <paulbg@hey.com>
2024-04-30 12:01:16 +0300

    feat: implement services section

---
+++ src/components/services.tsx
@@ -0,0 +1,20 @@
+import { Header } from "./header";
+import { ServicesList } from "../services/servicesList";
+import { services } from "@/data/services";
+
+/**
+ * This component abstracts the complexity of ServicesList and Service component.
+ * It represents the services section of the index page.
+ *
+ * @returns A react element that represents a section with a list of services
+ */
+export const Services = () => {
+  return (
+    <section className="grid mx-auto min-h-[50vh] max-w-5xl justify-center">
+      <article className="space-y-6">
+        <Header title="Services" description="Our Services" />
+        <ServicesList services={services} />
+      </article>
+    </section>
+  );
+};

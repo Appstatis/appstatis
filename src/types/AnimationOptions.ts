361445a50fd9e2ad6ca87d336456c7ede9883a0c
paulbg <paulbg@hey.com>
2024-05-02 11:29:11 +0300

    feat: AnimationOptions type
    This type will be used in a custom hook

---
+++ src/types/AnimationOptions.ts
@@ -0,0 +1,8 @@
+export type AnimationOptions = {
+  triggerOnce?: boolean;
+  threshold?: number;
+  translateY?: [number, number];
+  opacity?: [number, number];
+  duration?: number;
+  delay?: number;
+};

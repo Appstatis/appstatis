2c886e5baaac4f1c87f75736cf8b9263bd5315dc
paulbg <paulbg@hey.com>
2024-05-02 11:30:10 +0300

    chore: useAnimateInView hook in features

--- src/components/features/features.tsx
+++ src/components/features/features.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React, { useEffect, useContext } from "react";
+import React, { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import anime from "animejs";
 import { FeaturesList } from "@/components/features/FeaturesList";
 import { featuresList } from "../../data/featuresList";
-import ScrollContext from "@/context/scrollContext";
 
 /**
  * This component abstracts the complexity of FeaturesList and FeatureItem.
@@ -13,35 +12,8 @@ import ScrollContext from "@/context/scrollContext";
  * @returns A react element that represents a section with features
  */
 export const Features = () => {
-  const contextRef = useContext(ScrollContext);
-
-  const { ref: inViewRef, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.5,
-  });
-
-  const setRefs = (node: HTMLElement | null) => {
-    inViewRef(node);
-    if (node) contextRef.current = node;
-  };
-
-  useEffect(() => {
-    if (inView) {
-      anime({
-        targets: ".features",
-        translateY: [100, 0],
-        opacity: [0, 1],
-        duration: 3000,
-      });
-    }
-  }, [inView]);
-
   return (
-    <section
-      ref={setRefs}
-      id="features"
-      className="min-h-[75vh] opacity-0 features"
-    >
+    <section>
       <FeaturesList featuresList={featuresList} />
     </section>
   );

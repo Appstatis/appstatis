61c9e3b7fff112573f8c8a66c3c3913d715d62b1
paulbg <paulbg@hey.com>
2024-05-04 12:56:21 +0300

    feat: add descriptive icons and text to features list

--- src/data/featuresList.ts
+++ src/data/featuresList.ts
@@ -1,35 +1,48 @@
-import { SunIcon } from "@radix-ui/react-icons";
+import {
+  ChatBubbleIcon,
+  LockOpen2Icon,
+  LinkBreak2Icon,
+  HeartIcon,
+  ReloadIcon,
+  Pencil2Icon,
+} from "@radix-ui/react-icons";
 import { Feature } from "@/types/Feature";
 
 export const featuresList: Feature[] = [
   {
-    Icon: SunIcon,
-    title: "b",
-    description: "c",
+    Icon: LockOpen2Icon,
+    title: "Transparent Processes",
+    description:
+      "We promise full transparency in all our dealings, giving you insight into every stage of our process.",
   },
   {
-    Icon: SunIcon,
-    title: "b",
-    description: "c",
+    Icon: ChatBubbleIcon,
+    title: "Flexible Communication",
+    description:
+      "We prioritize efficient, meeting-free interactions, but are ready to meet when needed.",
   },
   {
-    Icon: SunIcon,
-    title: "b",
-    description: "c",
+    Icon: Pencil2Icon,
+    title: "Tailored Solutions",
+    description:
+      "Understanding your needs is our top priority. We promise personalized solutions that are tailored to meet your unique challenges and objectives.",
   },
   {
-    Icon: SunIcon,
-    title: "b",
-    description: "c",
+    Icon: LinkBreak2Icon,
+    title: "Innovation and Excellence",
+    description:
+      "We utilize modern, sleek, and optimized technologies to build superior web applications that offer the best performance in the market.",
   },
   {
-    Icon: SunIcon,
-    title: "b",
-    description: "c",
+    Icon: ReloadIcon,
+    title: "Unlimited Requests",
+    description:
+      "We offer unlimited support—no matter how many requests you have, we're here to make each one happen.",
   },
   {
-    Icon: SunIcon,
-    title: "b",
-    description: "c",
+    Icon: HeartIcon,
+    title: "Support and Aftercare",
+    description:
+      "We promise ongoing support and aftercare, ensuring that you continue to benefit from our services long after the initial purchase.",
   },
 ];

0daf38c2a8515b1b3a3c54d0faf5ea7ff8efdd37
paulbg <paulbg@hey.com>
2024-05-08 15:52:16 +0300

    feat: add flexible pricing plan

--- src/data/pricing.ts
+++ src/data/pricing.ts
@@ -3,10 +3,11 @@ import { PricingItem } from "@/types/PricingItem";
 export const pricing: PricingItem[] = [
   {
     title: "One Time",
-    cost: 1000,
+    cost: 600,
     description: "Tailored for quick resolutions without ongoing obligations.",
     type: "one-time",
     features: [
+      "5 pages.",
       "All-inclusive technical support.",
       "Post-launch support at $20/hr.",
       "Excludes third-party expenses.",
@@ -18,11 +19,17 @@ export const pricing: PricingItem[] = [
     description: "Ideal for small businesses and startups.",
     type: "periodic",
     features: [
+      "5 pages.",
       "Comprehensive technical support.",
       "Free hosting and domain registration.",
       "Continuous maintenance and proactive support.",
       "Bug fixes and performance enhancements.",
       "Note: Additional charges for third-party services may apply.",
     ],
+  },
+  {
+    title: "Custom",
+    description:
+      "Have a unique project in mind? Let's discuss your requirements and come up with a custom plan that suits your needs.",
   },
 ];

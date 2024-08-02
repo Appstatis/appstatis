a1766626c74bc12a2dff652d4df09880088d80b0
paulbg <paulbg@hey.com>
2024-08-02 10:37:10 +0300

    feat: translate Hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,3 +1,4 @@
+import useTranslation from "next-translate/useTranslation";
 import { CTA } from "@/components/hero/CTA";
 import { TextGenerateEffect } from "../ui/text-generate-effect";
 
@@ -7,12 +8,14 @@ import { TextGenerateEffect } from "../ui/text-generate-effect";
  * @returns A hero section that contains a message and a call to action component
  */
 export const Hero = () => {
+  const { t } = useTranslation("common");
+
   return (
     <>
       <section className="h-[80vh] grid justify-center place-content-center">
         <article className="space-y-5 max-w-2xl">
           <header className="font-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-            <TextGenerateEffect words="Unleash ideas Embrace innovation" />
+            <TextGenerateEffect words={t("hero.title")} />
           </header>
           <CTA />
         </article>

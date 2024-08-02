a609c1b60bf6f22d07a18d5c15e0b41ed1393304
paulbg <paulbg@hey.com>
2024-08-02 10:36:28 +0300

    feat: translate about header

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import useTranslation from "next-translate/useTranslation";
 import { useContext } from "react";
 import ScrollContext from "@/context/scrollContext";
 import { Header } from "../header";
 import { AboutContent } from "./about-content";
 
 export const About = () => {
+  const { t } = useTranslation("common");
   const { about } = useContext(ScrollContext);
 
   return (
     <section ref={about} className="min-h-[45vh] max-w-3xl mx-auto space-y-10">
-      <Header title="What is appstatis?" />
+      <Header title={t("about.title")} />
 
       <AboutContent />
     </section>

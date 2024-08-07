ce8e59544c46af7dfe6fae79bd3f02900045e599
paulbg <paulbg@hey.com>
2024-08-07 11:41:50 +0300

    feat: translate features

--- src/components/features/features.tsx
+++ src/components/features/features.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import useTranslation from "next-translate/useTranslation";
 import React from "react";
 import { FeaturesList } from "./featuresList";
 import { featuresList } from "../../data/featuresList";
+import { featuresIcons } from "@/data/featuresIcons";
+import { Feature } from "@/types/Feature";
 
 /**
  * This component abstracts the complexity of FeaturesList and FeatureItem.
@@ -10,9 +13,21 @@ import { featuresList } from "../../data/featuresList";
  * @returns A react element that represents a section with features
  */
 export const Features = () => {
+  const { t } = useTranslation("common");
+
+  const features = t(
+    "features.options",
+    {},
+    { returnObjects: true }
+  ) as Feature[];
+
+  features.forEach((feature) => {
+    feature.Icon = featuresIcons[feature.id];
+  });
+
   return (
     <section>
-      <FeaturesList featuresList={featuresList} />
+      <FeaturesList featuresList={features} />
     </section>
   );
 };

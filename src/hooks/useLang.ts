ab70ca096642e53fb22f492373141e0c2cd18a60
paulbg <paulbg@hey.com>
2024-08-07 13:01:49 +0300

    feat: update useLang to use more than 2 languages

--- src/hooks/useLang.ts
+++ src/hooks/useLang.ts
@@ -1,9 +1,12 @@
 import { useSearchParams } from "next/navigation";
 
 export const useLang = () => {
-  const searchParams = useSearchParams();
-  const lang = searchParams.get("lang") ?? "en";
-  const langToSwitch = lang === "en" ? "ee" : "en";
+  let languages = ["en", "ee", "ru"];
 
-  return { lang, langToSwitch };
+  const searchParams = useSearchParams();
+  const curLang = searchParams.get("lang") ?? "en";
+
+  languages = languages.filter((lang) => lang !== curLang);
+
+  return { curLang, languages };
 };

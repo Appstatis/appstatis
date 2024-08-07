d616b3a237b8ac231eb93b3a0e429dda168b0389
paulbg <paulbg@hey.com>
2024-08-07 10:32:55 +0300

    chore: add id to service

--- src/types/Service.ts
+++ src/types/Service.ts
@@ -1,6 +1,7 @@
 import { IconProps } from "@radix-ui/react-icons/dist/types";
 
 export type Service = {
+  id: string;
   Icon: React.ComponentType<IconProps>;
   title: string;
   description: string;
